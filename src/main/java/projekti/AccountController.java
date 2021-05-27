feb3d5902ec381c502389c9b7fc079dc7a396e13
asnabryg <8mixu8@gmail.com>
2021-05-27 18:12:10 +0300

    ulos kirjautuminen toimii

--- src/main/java/projekti/AccountController.java
+++ src/main/java/projekti/AccountController.java
@@ -9,6 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpSession;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Controller
 public class AccountController {
@@ -18,12 +22,18 @@ public class AccountController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    
+
     @Autowired
     private HttpSession session;
-    
+
     @GetMapping("/login")
-    public String login(Model model, @RequestParam Map<String, String> params){
+    public String login(Model model, @RequestParam Map<String, String> params) {
+        if (checkIfLoggedIn()) {
+            if (params.containsKey("logout")) {
+                return "login";
+            }
+            return "redirect:/index";
+        }
         if (params.containsKey("error")) {
             model.addAttribute("error", params.get("error").equals("true"));
         }
@@ -32,6 +42,9 @@ public class AccountController {
 
     @GetMapping("/register")
     public String register(Model model) {
+        if (checkIfLoggedIn()) {
+            return "redirect:/index";
+        }
         model.addAttribute("accounts", accountService.findAll());
         if (session.getAttribute("success") == null) {
             session.setAttribute("success", 0);
@@ -44,12 +57,20 @@ public class AccountController {
     @PostMapping("register")
     public String postRegister(@RequestParam String username, @RequestParam String password,
             @RequestParam String nickname, @RequestParam String passwordConfirm) {
+        if (checkIfLoggedIn()) {
+            return "redirect:/index";
+        }
         if (accountService.getAccount(username, false) != null) {
             session.setAttribute("success", "Username is already used!");
             return "redirect:/register";
         }
+        System.out.println("Nickname: " + nickname);
         if (!password.equals(passwordConfirm)) {
             session.setAttribute("success", "Passwords don't match!");
+            return "redirect:/register";
+        }
+        if (username.equals("") || nickname.equals("") || password.equals("") || passwordConfirm.equals("")) {
+            session.setAttribute("success", "Fill all fields!");
             return "redirect:/register";
         }
         Account account = new Account();
@@ -60,11 +81,16 @@ public class AccountController {
         session.setAttribute("success", 1);
         return "redirect:/register";
     }
-    
+
     @GetMapping("/clear")
-    public String clear(){
+    public String clear() {
         session.invalidate();
         return "redirect:/register";
+    }
+
+    private boolean checkIfLoggedIn() {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        return auth.getPrincipal() instanceof UserDetails;
     }
 
 }
