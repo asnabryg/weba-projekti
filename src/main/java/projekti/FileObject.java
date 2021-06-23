3e6ab2ae7a8234b53e48efd8c930cc44121e05e0
asnabryg <8mixu8@gmail.com>
2021-06-23 15:43:34 +0300

    .

--- src/main/java/projekti/FileObject.java
+++ src/main/java/projekti/FileObject.java
@@ -2,15 +2,13 @@
 package projekti;
 
 import java.util.List;
-import javax.persistence.Basic;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Lob;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
 import org.springframework.data.jpa.domain.AbstractPersistable;
 
 @Entity
@@ -33,8 +31,9 @@ public class FileObject extends AbstractPersistable<Long>{
     @OneToMany(mappedBy="file")
     private List<ImageComment> comments;
     
-    //    @Basic(fetch = FetchType.LAZY)
-    @Lob
+//    @Lob
+//    @Basic(fetch = FetchType.LAZY)
+    @Type(type="org.hibernate.type.BinaryType")
     private byte[] content;
     
 }
